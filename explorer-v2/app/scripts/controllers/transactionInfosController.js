3e639096106873f96cd5f845122a015f5a09fab5
Serge Koba <s.koba@mobidev.biz>
2018-01-07 19:25:29 +0200

    use explorer v2

--- explorer-v2/app/scripts/controllers/transactionInfosController.js
+++ explorer-v2/app/scripts/controllers/transactionInfosController.js
@@ -1,11 +1,14 @@
 angular.module('ethExplorer')
     .controller('transactionInfosCtrl', function ($rootScope, $scope, $location, $routeParams,$q) {
 
-       var web3 = $rootScope.web3;
-
         $scope.init=function()
         {
+
+
             $scope.txId=$routeParams.transactionId;
+
+
+
 
             if($scope.txId!==undefined) { // add a test to check if it match tx paterns to avoid useless API call, clients are not obliged to come from the search form...
 
@@ -30,18 +33,16 @@ angular.module('ethExplorer')
                     }
                     $scope.from = result.from;
                     $scope.gas = result.gas;
-                    $scope.gasUsed = result.gasUsed;
-                    $scope.contractAddress = result.contractAddress;
-                    $scope.gasPrice = result.gasPrice.c[0] + " WEI";
+                    //$scope.gasPrice = result.gasPrice.c[0] + " WEI";
+                    $scope.gasPrice = web3.fromWei(result.gasPrice, "ether").toFormat(10) + " ETH";
                     $scope.hash = result.hash;
                     $scope.input = result.input; // that's a string
-                    $scope.decoded = decodeData(result.input);
                     $scope.nonce = result.nonce;
                     $scope.to = result.to;
                     $scope.transactionIndex = result.transactionIndex;
+                    //$scope.ethValue = web3.fromWei(result.value[0], "ether"); Newer method but has ""
                     $scope.ethValue = result.value.c[0] / 10000;
-                    console.log(result);
-                    $scope.txprice = (result.gas * result.gasPrice)/1000000000000000000 + " ETH";
+                    $scope.txprice = web3.fromWei(result.gas * result.gasPrice, "ether") + " ETH";
                     if($scope.blockNumber!==undefined){
                         $scope.conf = number - $scope.blockNumber;
                         if($scope.conf===0){
@@ -52,7 +53,7 @@ angular.module('ethExplorer')
                     if($scope.blockNumber!==undefined){
                         var info = web3.eth.getBlock($scope.blockNumber);
                         if(info!==undefined){
-                            $scope.time = info.timestamp + '000';
+                            $scope.time = info.timestamp;
                         }
                     }
 
@@ -72,12 +73,7 @@ angular.module('ethExplorer')
 
                 web3.eth.getTransaction($scope.txId,function(error, result) {
                     if(!error){
-                        web3.eth.getTransactionReceipt($scope.txId,function(err2, receipt) {
-                            if(!err2) {
-                                for (var attrname in receipt) { result[attrname] = receipt[attrname]; }
-                            }
-                            deferred.resolve(result);
-                        });
+                        deferred.resolve(result);
                     }
                     else{
                         deferred.reject(error);
@@ -86,6 +82,8 @@ angular.module('ethExplorer')
                 return deferred.promise;
 
             }
+
+
 
         };
         $scope.init();
